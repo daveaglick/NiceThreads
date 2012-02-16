8150960bc92ae2988d63fc3bbc5edd1640e8e97a
somedave <dave@somedave.com>
2012-02-16 15:16:49 -0500

    Licensing updates.

--- NiceThreads/Properties/AssemblyInfo.cs
+++ NiceThreads/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NiceThreads")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DRAC")]
+[assembly: AssemblyCompany("WildCard, LLC")]
 [assembly: AssemblyProduct("NiceThreads")]
-[assembly: AssemblyCopyright("Copyright © DRAC 2012")]
+[assembly: AssemblyCopyright("Copyright © WildCard, LLC 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
